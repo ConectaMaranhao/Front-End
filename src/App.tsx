f745dec793cad4f30b18d8f1508ceef63cd2f04d
Nadson-Cutrim <nadsoncutrimjunior@aluno.uema.br>
2025-06-17 01:03:58 -0300

    ⭐feat(App.tsx): Adicionando Route do about

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import EmpresasPage from './pages/Empresas/EmpresasPage';
+import { AboutPage } from './pages/About/AboutPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/empresas" element={<EmpresasPage />} />
+        <Route path='/about' element={<AboutPage/>} />
         {/* outras rotas */}
       </Routes>
     </Router>
