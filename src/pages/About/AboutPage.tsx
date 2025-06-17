9fb09bb64292a1512cab2d831da94cd8568e65b9
Nadson-Cutrim <nadsoncutrimjunior@aluno.uema.br>
2025-06-17 00:39:00 -0300

    ⭐feat(about): Criação da Seção "Quem Somos Nós"

--- src/pages/About/AboutPage.tsx
+++ src/pages/About/AboutPage.tsx
@@ -1,1 +1,25 @@
-//Tela "Sobre o Projeto".
+import React from 'react';
+import { AboutSection } from '../../components/About/AboutSection/AboutSection';
+
+import timeImage from '../../assets/About/dev-time.png'; 
+import inovaLogo from '../../assets/About/logo-inova.png';
+import { SectionDestaque } from '../../components/About/DestaqueSection/DestaqueSection';
+
+export const AboutPage: React.FC = () => {
+  const aboutText = "O Conecta Maranhão é uma iniciativa inovadora que nasceu com o propósito de transformar vidas por meio da capacitação profissional e da inclusão produtiva. Somos uma plataforma digital gratuita que integra talentos maranhenses a oportunidades concretas no mercado de trabalho.";
+
+  return (
+    <div>
+      {/* NavBar... */}
+      
+      <AboutSection
+        title="QUEM SOMOS NÓS?"
+        text={aboutText}
+        mainImage={timeImage}
+        logoImage={inovaLogo}
+      />
+<SectionDestaque />
+      {/* Footer... */}
+    </div>
+  );
+};
