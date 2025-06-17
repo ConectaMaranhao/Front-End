44bf4da07a3dc42d008aa6c241bf3d3e0184bf98
Nadson-Cutrim <nadsoncutrimjunior@aluno.uema.br>
2025-06-17 00:59:08 -0300

    ⭐feat(about): adiciona seção de Missão, Visão e Valores - Cria o componente reutilizável DestaqueCard para exibir um ícone, título e texto. - Desenvolve a seção principal DestaqueSection que organiza os cards. - Integra a biblioteca react-icons para a representação visual dos destaques.

---
+++ src/components/About/AboutSection/AboutSection.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './AboutSection.module.css'; // Importando nossos estilos
+import type { IAbout } from '../../../interfaces/IAbout';
+
+export const AboutSection: React.FC<IAbout> = ({
+  title,
+  text,
+  mainImage,
+  logoImage,
+}) => {
+  return (
+    <section className={styles.heroContainer}>
+      <div className={styles.textColumn}>
+        <h1>{title}</h1>
+        <p>{text}</p>
+      </div>
+      <div className={styles.imageColumn}>
+        <img 
+          src={mainImage} 
+          alt="Equipe do projeto Conecta Maranhão" 
+          className={styles.mainImage}
+        />
+        <img 
+          src={logoImage} 
+          alt="Logo do parceiro Inova Maranhão" 
+          className={styles.logoImage}
+        />
+      </div>
+    </section>
+  );
+};
