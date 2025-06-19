92861b06993cbed23e9e8ab028ae116c25ac8905
Nadson-Cutrim <nadsoncutrimjunior@aluno.uema.br>
2025-06-19 00:05:53 -0300

    ⭐feat(src) Criando paginas base

--- src/pages/Cursos/CursosPage.tsx
+++ src/pages/Cursos/CursosPage.tsx
@@ -1,1 +1,14 @@
-//Tela de listagem  de cursos.
+//Tela de listagem  de cursos.import React from 'react';
+
+export const CursosPage: React.FC = () => {
+  return (
+    <div 
+    style={{ 
+    padding: '2rem',
+    textAlign: 'center' 
+     }}>
+      <h1>Cursos</h1>
+      <p>Confira os cursos disponíveis em nossa plataforma.</p>
+    </div>
+  );
+};
---
+++ src/pages/Home/HomePage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const HomePage: React.FC = () => {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Página Inicial</h1>
+      <p>Bem-vindo ao Conecta Maranhão!</p>
+    </div>
+  );
+};
