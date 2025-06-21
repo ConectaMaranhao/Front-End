74c7e7d09d590b9324d2912746850ff9b1be3279
Nadson-Cutrim <nadsoncutrimjunior@aluno.uema.br>
2025-06-21 12:54:03 -0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base:"/Front-End/"
-=======
-  base:"/Front-End"
->>>>>>> f579ff351efcf4dec39e9b59b5a81d6ccaf51c95
 })
