53e31671d357ebe3c90b54bf58d4f77ce1edebe4
Nadson-Cutrim <nadsoncutrimjunior@aluno.uema.br>
2025-06-21 13:26:48 -0300

    ⭐feat (main): Ajustando rotas

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>  
+    <BrowserRouter basename='/Front-End/'>  
       <App />
     </BrowserRouter> 
   </React.StrictMode>
