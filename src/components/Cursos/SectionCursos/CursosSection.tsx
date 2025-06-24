8baa1f732df4f12154cf6e9712189ae6e9dda139
Nadson-Cutrim <nadsoncutrimjunior@aluno.uema.br>
2025-06-24 00:50:13 -0300

    ⭐feat(cursos): cria a seção de hero da página de Cursos

---
+++ src/components/Cursos/SectionCursos/CursosSection.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './CursosSection.module.css';
+
+const filtros = ['Todos', 'Front-End', 'Back-End', 'UX Design', 'Promoção', 'Jogos', 'Análise de Dados'];
+
+export const CursosSection: React.FC = () => {
+  return (
+    <section className={styles.cursosContainer}>
+      <h1>Cursos em Tecnologia</h1>
+      <p>Desenvolva suas habilidades com nossos cursos nas áreas de Front-End, Back-End, UX Design, promoção, jogos e análise de dados.</p>
+      <div className={styles.filtrosContainer}>
+        {filtros.map(filtros => (
+          <button key={filtros} className={styles.filtroButton}>{filtros}</button>
+        ))}
+      </div>
+      <button className={styles.ctaButton}>Ver Cursos</button>
+    </section>
+  );
+};
