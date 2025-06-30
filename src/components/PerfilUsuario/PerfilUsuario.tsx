b90ea6d98d3265e31d870c58510ef5ff58eb96c2
Nadson-Cutrim <nadsoncutrimjunior@aluno.uema.br>
2025-06-29 22:30:08 -0300

    ⭐feat: Adicionando NavBar no Perfil do Usuário

--- src/components/PerfilUsuario/PerfilUsuario.tsx
+++ src/components/PerfilUsuario/PerfilUsuario.tsx
@@ -5,18 +5,19 @@ import PerfilHeaderSecao from "./PerfilHeader/PerfilHeaderSecao";
 import ProgressoSecao from "./Progresso/ProgressoSecao";
 import FormularioSecao from "./Formulario/FormularioSecao";
 import MedalhasSecao from "./Medalhas/MedalhasSecao";
-import { buscarPessoaPorId } from '../../services/api';
+import { buscarPessoaPorId } from "../../services/api";
+import { NavBar } from "../Layout/NavBar/NavBar";
 
 export const PerfilUsuario: React.FC = () => {
   const [pessoa, setPessoa] = useState<any>(null);
-  const pessoaId = localStorage.getItem('pessoaId');
-  const userId = localStorage.getItem('userId');
+  const pessoaId = localStorage.getItem("pessoaId");
+  const userId = localStorage.getItem("userId");
 
   // ref pra seção formulário
   const formularioRef = useRef<HTMLDivElement>(null);
 
   const rolarParaFormulario = () => {
-    formularioRef.current?.scrollIntoView({ behavior: 'smooth' });
+    formularioRef.current?.scrollIntoView({ behavior: "smooth" });
   };
 
   useEffect(() => {
@@ -26,16 +27,24 @@ export const PerfilUsuario: React.FC = () => {
         setPessoa(res.data);
       } else if (userId) {
         // 1. Buscar contas e encontrar a do usuário logado
-        const contasRes = await import('../../services/api').then(m => m.api.get('/contas'));
+        const contasRes = await import("../../services/api").then((m) =>
+          m.api.get("/contas")
+        );
 
-        const conta = contasRes.data.find((c: any) => c.userId && c.userId._id === userId);
+        const conta = contasRes.data.find(
+          (c: any) => c.userId && c.userId._id === userId
+        );
 
         if (conta) {
           // 2. Buscar pessoas e encontrar a pessoa que tem a conta
-          const pessoasRes = await import('../../services/api').then(m => m.api.get('/pessoas'));
+          const pessoasRes = await import("../../services/api").then((m) =>
+            m.api.get("/pessoas")
+          );
 
-          const pessoa = pessoasRes.data.find((p: any) => p.contaId === conta._id);
-          
+          const pessoa = pessoasRes.data.find(
+            (p: any) => p.contaId === conta._id
+          );
+
           if (pessoa) setPessoa(pessoa);
         }
       }
@@ -47,26 +56,11 @@ export const PerfilUsuario: React.FC = () => {
 
   return (
     <div className={styles.container}>
-      {/*
-      Barra Superior
-      <div className={styles.barraSuperior}>
-        <div className={styles.iconeAvatar} />
-        <div className={styles.titulo}>Meu Perfil</div>
-        <div className={styles.navegacao}>
-          <div className={styles.aba}>Início</div>
-          <div className={styles.aba}>Meus cursos</div>
-          {/*  <div className={styles.campoBusca}>
-            <div className={styles.textoBusca}>Search in site</div>
-            <img
-              src="/search-icon.png"
-              alt="Search"
-              className={styles.iconeBusca}
-            />
-          </div>
-        </div>
-      </div> */}
-
-      <PerfilHeaderSecao pessoa={pessoa} aoClicarEditarPerfil={rolarParaFormulario} />
+      <NavBar />
+      <PerfilHeaderSecao
+        pessoa={pessoa}
+        aoClicarEditarPerfil={rolarParaFormulario}
+      />
 
       <ProgressoSecao xp={pessoa.xp} />
 
